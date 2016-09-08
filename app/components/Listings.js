6b1f8f0adb325a178c4cce7b4d8477ae59cb4648
Poksay <poksay.andrew@gmail.com>
2016-09-08 07:20:54 -0700

    AJAX request attempts to run but CORS issue exists

--- app/components/Listings.js
+++ app/components/Listings.js
@@ -1,9 +1,8 @@
 import React from 'react';
 // import Axios from 'axios';
 
-class Listing extends React.Component{
-
-  render () {
+var Listing = React.createClass({
+  render: function() {
     return(
       <div className="listing-container">
         <div className="listing-header">
@@ -12,11 +11,12 @@ class Listing extends React.Component{
         </div>
         <img id="listing-image" src={this.props.picture}></img>
       </div>
-    )};
-};
+    );
+  }
+});
 
-class ListingBox extends React.Component{
-  loadListingsFromServer() {
+var ListingBox = React.createClass({
+  loadListingsFromServer: function() {
     $.ajax({
       url: this.props.url,
       dataType: 'json',
@@ -28,39 +28,40 @@ class ListingBox extends React.Component{
         console.error(this.props.url, status, err.toString());
       }.bind(this)
     });
-  };
-  getInitialState() {
+  },
+
+  getInitialState: function() {
     return {data: []};
-  };
-  componentDidMount() {
+  },
+  componentDidMount: function() {
     this.loadListingsFromServer();
-  };
+  },
   // Set state of component to pass to ListingList component
   // function to get Listing data
   // render the Lisitng List with a title
-  render() {
+  render: function() {
     return (
       <div className="container">
         <nav> SurfSwap </nav>
         <Listing />
       </div>
-    )};
-};
+    )}
+});
 
-// class ListingList extends React.Component{
-//   render() {
-//     var listingNodes = this.props.data.map(function(listing) {
-//       return (
-//         <Listing name={listing.name} price={listing.price} picture={listing.picture}>
-//         <Listing />
-//       );
-//     });
-//     return (
-//       <div className="listingList">
-//         {listingNodes}
-//       </div>
-//     );
-//   }
-// };
+var ListingList = React.createClass({
+  render: function() {
+    var listingNodes = this.props.data.map(function(listing) {
+      return (
+        <Listing name={listing.name} price={listing.price} picture={listing.picture}>
+        </Listing>
+      );
+    });
+    return (
+      <div className="listingList">
+        {listingNodes}
+      </div>
+    );
+  }
+});
 
 export default ListingBox;
